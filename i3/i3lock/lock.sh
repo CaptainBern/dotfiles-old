22c2b25c641a7545221f85ade46c475d245f4125
CaptainBern <captainberneby@gmail.com>
2016-11-04 22:53:11 +0100

    Ahh simplicty

--- i3/i3lock/lock.sh
+++ i3/i3lock/lock.sh
@@ -23,34 +23,14 @@ cleanup() {
 
 lock() {
   scrot $IMAGE
-  convert $IMAGE -scale 10% -scale 1000% $IMAGE
+  convert $IMAGE -level 0%,100%,0.6 -scale 10% -scale 1000% $IMAGE
 
   if [[ -f $LOCK_ICON ]]
   then
-      # placement x/y
-      PX=0
-      PY=0
-      # lockscreen image info
-      R=$(file $LOCK_ICON | grep -o '[0-9]* x [0-9]*')
-      RX=$(echo $R | cut -d' ' -f 1)
-      RY=$(echo $R | cut -d' ' -f 3)
-
-      SR=$(xrandr --query | grep ' connected' | cut -f3 -d' ')
-      for RES in $SR
-      do
-          # monitor position/offset
-          SRX=$(echo $RES | cut -d'x' -f 1)                   # x pos
-          SRY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 1)  # y pos
-          SROX=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 2) # x offset
-          SROY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 3) # y offset
-          PX=$(($SROX + $SRX/2 - $RX/2))
-          PY=$(($SROY + $SRY/2 - $RY/2))
-
-          convert $IMAGE -level 0%,100%,0.6 $LOCK_ICON -geometry +$PX+$PY -composite -matte $IMAGE
-      done
+      convert $IMAGE $LOCK_ICON -gravity center -composite $IMAGE
   fi
 
-  i3lock -n -u -t -i $IMAGE
+  i3lock -n -u -f -t -i $IMAGE
 }
 
 main() {
